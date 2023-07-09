80df2b87836a11faa2f85ddf4f1a0ebe0c43f4ab
deividbizerra <souzadeivid326@gmail.com>
2023-07-09 15:32:11 -0300

    finalizado o cadstro de nova turma JS

---
+++ ProjetoFinal/Src/JS/Turmas JS/novaturma.js
@@ -0,0 +1,141 @@
+// Seleciona o formulário pelo seu ID
+const formNovaTurma = document.querySelector("#formNovaTurma");
+
+// Função assíncrona para buscar um mentor pelo ID
+const buscarMentoresId = async (id) => {
+  if (id == null) {
+    return false;
+  }
+
+  const response = await fetch(`http://localhost:3000/mentores/${id}`);
+  const mentoresJson = await response.json();
+  return mentoresJson;
+};
+
+// Função assíncrona para buscar todos os mentores
+const buscarMentores = async () => {
+  const response = await fetch(`http://localhost:3000/mentores`);
+  const mentorJson = await response.json();
+  return mentorJson;
+};
+
+// Função para carregar as opções do select de mentores
+const carregarSelect = async () => {
+  const mentorias = await buscarMentores();
+  const mentoriaSelect = document.getElementById("mentor");
+
+  const opVazia = new Option("Selecione um mentor...", null);
+  mentoriaSelect.options.add(opVazia);
+
+  mentorias.forEach((mentor) => {
+    const opcoes = new Option(mentor.name, mentor.id);
+    mentoriaSelect.options.add(opcoes);
+  });
+};
+
+// Função assíncrona para buscar uma mentoria pelo ID
+const buscarMentoriasId = async (id) => {
+  if (id == null) {
+    return false;
+  }
+
+  const response = await fetch(`http://localhost:3000/mentorias/${id}`);
+  const mentoriasJson = await response.json();
+  return mentoriasJson;
+};
+
+// Função assíncrona para buscar todas as mentorias
+const buscarMentorias = async () => {
+  const response = await fetch(`http://localhost:3000/mentorias`);
+  const mentoriaJson = await response.json();
+  return mentoriaJson;
+};
+
+// Função para carregar as opções do select de mentorias
+const carregarSelectMentoria = async () => {
+  const mentorias = await buscarMentorias();
+  const mentoriaSelect = document.getElementById("tituloMentoria");
+
+  const opVazia = new Option("Selecione uma mentoria...", null);
+  mentoriaSelect.options.add(opVazia);
+
+  mentorias.forEach((mentorias) => {
+    const opcoes = new Option(mentorias.titulo, mentorias.id);
+    mentoriaSelect.options.add(opcoes);
+  });
+};
+
+// Função assíncrona para cadastrar uma nova turma
+const cadastrarNovaTurma = async (turmas) => {
+  try {
+    await fetch(`http://localhost:3000/turmas`, {
+      method: "POST",
+      headers: {
+        Accept: "application/json, text/plain, */*",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(turmas),
+    });
+    window.location = "../Turmas/turmas.html";
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// Adiciona um listener de evento para o evento "submit" do formulário
+formNovaTurma.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  // Obtém os valores dos campos do formulário
+  const diasDaSemana = formNovaTurma.elements["diasDaSemana"].value;
+  const horarioInicio = formNovaTurma.elements["horarioInicio"].value;
+  const horarioFim = formNovaTurma.elements["horarioFim"].value;
+  const turma = formNovaTurma.elements["turma"].value;
+  const linkAula = formNovaTurma.elements["linkAula"].value;
+  const qtdEncontro = formNovaTurma.elements["qtdEncontro"].value;
+  const dataInicio = formNovaTurma.elements["dataInicio"].value;
+
+  // Obtém o valor selecionado do select de mentor
+  const mentorSelect = document.getElementById("mentor");
+  const mentorOption = mentorSelect.options[mentorSelect.selectedIndex];
+  const mentorName = mentorOption.text;
+
+  // Obtém o valor selecionado do select de mentoria
+  const mentoriaSelect = document.getElementById("tituloMentoria");
+  const mentoriaOption = mentoriaSelect.options[mentoriaSelect.selectedIndex];
+  const mentoriaName = mentoriaOption.text;
+
+  if (
+    diasDaSemana &&
+    horarioInicio &&
+    horarioFim &&
+    turma &&
+    linkAula &&
+    qtdEncontro &&
+    dataInicio &&
+    mentorName &&
+    mentoriaName !== ""
+  ) {
+    // Cria um objeto com os valores dos campos do formulário
+    const turmas = {
+      turma,
+      name: mentorName,
+      mentoria: mentoriaName,
+      diaSemana: diasDaSemana,
+      horario: {
+        horaInicio: horarioInicio,
+        horarioFim: horarioFim,
+      },
+      encontros: qtdEncontro,
+      linkAula,
+      dataInicio,
+    };
+    cadastrarNovaTurma(turmas);
+  } else {
+    alert("Preencha todos os campos");
+  }
+});
+
+// Chama as funções para carregar os selects
+carregarSelectMentoria();
+carregarSelect();
--- ProjetoFinal/Src/JS/Turmas JS/turmas.js
+++ ProjetoFinal/Src/JS/Turmas JS/turmas.js
@@ -1,90 +1,97 @@
-const tbody = document.querySelector("tbody")
+// Seleciona o elemento <tbody> do HTML
+const tbody = document.querySelector("tbody");
 
+// Função assíncrona para buscar as turmas
+const buscarTumar = async () => {
+  try {
+    const response = await fetch("http://localhost:3000/turmas");
+    const turmaJson = await response.json();
+    mostrarTurma(turmaJson);
+  } catch (erro) {
+    console.log(erro);
+  }
+};
 
-const buscarTumar = async ()=>{
-    try{
-        const response = await fetch("http://localhost:3000/turmas")
-        const turmaJson = await response.json()
-        mostrarTurma(turmaJson)
-    }catch(erro){
-        console.log(erro)
-    }
-}
-
+// Função para redirecionar para a página de edição de uma turma
 const editarTuma = (id) => {
-    window.location = `editarmturma.html?id=${id}`;
-  };
+  window.location = `editarmturma.html?id=${id}`;
+};
 
-const mostrarTurma = (dados)=>{
-    tbody.innerHTML = ""
+// Função para exibir as turmas na tabela
+const mostrarTurma = (dados) => {
+  tbody.innerHTML = "";
 
-    dados.forEach(dados => {
-        tbody.innerHTML += `
+  dados.forEach((dados) => {
+    // Converte a data de início para o formato brasileiro
+    const dataInicio = new Date(dados.dataInicio);
+    const dataFormatada = dataInicio.toLocaleDateString("pt-BR");
+
+    // Cria uma nova linha na tabela com os dados da turma
+    tbody.innerHTML += `
         <tr>
           <td class="nameMentores">${dados.turma}</td>
           <td>${dados.name}</td>
           <td>${dados.mentoria}</td>
-          <td>${dados.dataInicio}</td>
+          <td>${dataFormatada}</td>
           <td>${dados.diaSemana}</td>
-          <td>${dados.horario}</td>
-          <td>${dados.encontros}</td>
+          <td><span>${dados.horario.horaInicio}</span> - <span>${dados.horario.horarioFim}</span></td>
+          <td>${dados.encontros}/10</td>
           <td class="icones">
-            <i class="fas fa-edit iMentorEditar" onclick="editarMentoria(${
-              dados.id
-            })"></i>
-            <i class="fas fa-trash iMentorExcluir" onclick="deletTurma(${
-              dados.id
-            })"></i>
+            <i class="fas fa-edit iMentorEditar" onclick="editarMentoria(${dados.id})"></i>
+            <i class="fas fa-trash iMentorExcluir" onclick="deletTurma(${dados.id})"></i>
           </td>
         </tr>
-        `
+        `;
+  });
+};
+
+// Chama a função buscarTumar para obter as turmas e exibi-las na tabela
+buscarTumar();
+
+// Função assíncrona para deletar uma turma
+const deletTurma = async (turmaId) => {
+  try {
+    await fetch(`http://localhost:3000/turmas/${turmaId}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
     });
-}
+  } catch (erro) {
+    console.log(erro);
+  }
+};
 
-buscarTumar()
-
-const deletTurma = async (turmaId)=>{
-    try{
-        await fetch (`http://localhost:3000/turmas/${turmaId}`,{
-            method: "DELETE",
-            headers:{
-                "Content-Type": "application/json",
-            }
-        })
-    }catch(erro){
-        console.log(erro)
-    }
-}
-
+// Função para redirecionar para a página de criação de nova turma
 const novaTurma = () => {
-    window.location = "../Turmas/novaturma.html";
-  };
+  window.location = "../Turmas/novaturma.html";
+};
 
-  const pesquisar = () => {
-    const digitado = inputSearch.value.toLowerCase();
-    // Obtém o valor digitado no input de busca em letras minúsculas e armazena na variável 'digitado'
-  
-    const itens = tbody.getElementsByTagName("tr");
-    // Obtém todos os elementos <tr> dentro do elemento <tbody> e armazena na variável 'itens'
-  
-    for (let posicao in itens) {
-      // Itera sobre os elementos 'itens' usando a variável 'posicao'
-  
-      if (true === isNaN(posicao)) {
-        continue;
-        // Verifica se 'posicao' não é um número (índice inválido) e pula para a próxima iteração
-      }
-  
-      let conteudoTabela = itens[posicao].innerHTML.toLowerCase();
-      // Obtém o conteúdo HTML do elemento <tr> atual em letras minúsculas e armazena na variável 'conteudoTabela'
-  
-      if (true === conteudoTabela.includes(digitado)) {
-        itens[posicao].style.display = "";
-        // Se o valor digitado estiver presente no conteúdo da tabela, mostra o elemento <tr>
-      } else {
-        itens[posicao].style.display = "none";
-        // Caso contrário, oculta o elemento <tr>
-      }
+// Função para pesquisar as turmas
+const pesquisar = () => {
+  const digitado = inputSearch.value.toLowerCase();
+  // Obtém o valor digitado no input de busca em letras minúsculas e armazena na variável 'digitado'
+
+  const itens = tbody.getElementsByTagName("tr");
+  // Obtém todos os elementos <tr> dentro do elemento <tbody> e armazena na variável 'itens'
+
+  for (let posicao in itens) {
+    // Itera sobre os elementos 'itens' usando a variável 'posicao'
+
+    if (true === isNaN(posicao)) {
+      continue;
+      // Verifica se 'posicao' não é um número (índice inválido) e pula para a próxima iteração
     }
-  };
-  
+
+    let conteudoTabela = itens[posicao].innerHTML.toLowerCase();
+    // Obtém o conteúdo HTML do elemento <tr> atual em letras minúsculas e armazena na variável 'conteudoTabela'
+
+    if (true === conteudoTabela.includes(digitado)) {
+      itens[posicao].style.display = "";
+      // Se o valor digitado estiver presente no conteúdo da tabela, mostra o elemento <tr>
+    } else {
+      itens[posicao].style.display = "none";
+      // Caso contrário, oculta o elemento <tr>
+    }
+  }
+};
