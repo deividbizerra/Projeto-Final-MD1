94ed654c2670f5d96f74f2b81a269d1f130c40e3
deividbizerra <souzadeivid326@gmail.com>
2023-07-13 20:44:32 -0300

    adicionado o cadastro de usuario

--- ProjetoFinal/Src/JS/login/login.js
+++ ProjetoFinal/Src/JS/login/login.js
@@ -1,19 +1,48 @@
 // Selecionando os elementos do formulário
-const formulario = document.querySelector("#Formlogin");
+const formulario = document.querySelector("#signin");
 const email = document.querySelector("#email");
 const senha = document.querySelector("#senha");
 const label = document.querySelector(".labelLogin");
 const idLabel = document.querySelector("#idLabel");
 const validacao = document.querySelector("#validacao");
+const signin = document.querySelector("#signin");
+const singup = document.querySelector("#singup");
+const btnColor = document.querySelector("#btnColor");
+const login = document.querySelector("#login");
+const cadastrar = document.querySelector("#cadastrar");
+const labelCadastr = document.querySelector("#labelCadastrNome");
+const Labelemail = document.querySelector("#labelCadastrEmail");
+const nomeCompleto = document.querySelector("#nomeCompleto");
+const emailCadastro = document.querySelector("#emailCadastr");
+const labelSenhaCadastr = document.querySelector("#labelSenhaCadastr");
+const senhaCadastr = document.querySelector("#senhaCadastr");
 
-// Adicionando um evento de envio ao formulário
-formulario.addEventListener("submit", (e) => {
+// Altera o estilo ao clicar no botão de login
+document.querySelector("#login").addEventListener("click", () => {
+  signin.style.left = "75px";
+  singup.style.left = "450px";
+  btnColor.style.left = "0px";
+  login.style.color = "white";
+  cadastrar.style.color = "black";
+});
+
+// Altera o estilo ao clicar no botão de cadastrar
+document.querySelector("#cadastrar").addEventListener("click", () => {
+  signin.style.left = "-450px";
+  singup.style.left = "75px";
+  btnColor.style.left = "110px";
+  login.style.color = "black";
+  cadastrar.style.color = "white";
+});
+
+// Validação do formulário de login
+formulario.addEventListener("submit", async (e) => {
   e.preventDefault(); // Impedindo o envio padrão do formulário
 
-  let emailValido = true; //Variavel para verificar se o email é valido
-  let senhaValida = true; //Variavel para verificar se a senha é valida
+  let emailValido = true; // Variável para verificar se o email é válido
+  let senhaValida = true; // Variável para verificar se a senha é válida
 
-  // Verificando se o campo de email está vazio
+  // Verifica se o campo de email está vazio e aplica as classes de validação
   if (email.value === "") {
     label.classList.add("invalidoLabel");
     email.classList.add("invalido");
@@ -25,7 +54,7 @@ formulario.addEventListener("submit", (e) => {
     email.classList.add("sucesso");
   }
 
-  // Verificando se o campo de senha está vazio ou preenchida para adicionar as classes de validação
+  // Verifica se o campo de senha está vazio e aplica as classes de validação
   if (senha.value === "") {
     idLabel.classList.add("invalidoLabel");
     senha.classList.add("invalido");
@@ -37,23 +66,119 @@ formulario.addEventListener("submit", (e) => {
     senha.classList.add("sucesso");
   }
 
-  // Verificando se tanto o email quanto a senha são válidos
+  // Verifica se tanto o email quanto a senha são válidos
   if (emailValido && senhaValida) {
-    validacao.style.display = "none"; // Ocultando a mensagem de validação
+    validacao.style.display = "none"; // Oculta a mensagem de validação
 
-    // Verificando se o email e senha correspondem aos valores esperados
-    if (email.value == "admin@gmail.com" && senha.value == "admin") {
-      // Redirecionando para a página "telainicial.html" após 1 segundo
+    // Realiza a validação dos dados com sua API de usuários
+    const usuarios = await buscarUsuarios(); // Supondo que você já tenha uma função para buscar os usuários da API
+
+    // Verifica se o email e a senha correspondem aos valores esperados
+    const usuarioEncontrado = usuarios.find(
+      (usuario) =>
+        usuario.email === email.value && usuario.senha === senha.value
+    );
+
+    if (usuarioEncontrado) {
+      // Redireciona para a página "telainicial.html" após 1 segundo
       setTimeout(function () {
         window.location = "Html/Mentores/mentores.html";
       }, 1000);
     } else {
-      // Exibindo a mensagem de validação e adicionando classes de estilo inválido aos elementos
+      // Exibe a mensagem de validação e aplica as classes de estilo inválido aos elementos
       label.classList.add("invalidoLabel");
       email.classList.add("invalido");
       idLabel.classList.add("invalidoLabel");
       senha.classList.add("invalido");
       validacao.style.display = "block";
     }
+  }
+});
+
+// Função assíncrona para cadastrar um usuário
+const cadastrarUsuario = async (usuarios) => {
+  try {
+    // Faz uma requisição POST para a API para cadastrar o usuário
+    await fetch(`http://localhost:3000/usuarios`, {
+      method: "POST",
+      headers: {
+        Accept: "application/json, text/plain, */*",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(usuarios),
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// Função assíncrona para buscar os usuários da API
+const buscarUsuarios = async () => {
+  try {
+    // Faz uma requisição GET para a API para buscar os usuários
+    const response = await fetch(`http://localhost:3000/usuarios`);
+    const usuariosJson = await response.json();
+    return usuariosJson;
+  } catch (error) {
+    console.log(error);
+    return [];
+  }
+};
+
+// Validação do formulário de cadastro
+singup.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const nome = nomeCompleto.value;
+  const email = emailCadastro.value;
+  const senha = senhaCadastr.value;
+
+  let emailValido = true; // Variável para verificar se o email é válido
+  let senhaValida = true; // Variável para verificar se a senha é válida
+
+  // Verifica se o campo de nome está vazio e aplica as classes de validação
+  if (nome === "") {
+    labelCadastr.classList.add("invalidoLabel");
+    nomeCompleto.classList.add("invalido");
+    emailValido = false;
+  } else {
+    labelCadastr.classList.remove("invalidoLabel");
+    nomeCompleto.classList.remove("invalido");
+    labelCadastr.classList.add("sucessoLabel");
+    nomeCompleto.classList.add("sucesso");
+  }
+
+  // Verifica se o campo de email está vazio e aplica as classes de validação
+  if (email === "") {
+    Labelemail.classList.add("invalidoLabel");
+    emailCadastro.classList.add("invalido");
+    senhaValida = false;
+  } else {
+    Labelemail.classList.remove("invalidoLabel");
+    emailCadastro.classList.remove("invalido");
+    Labelemail.classList.add("sucessoLabel");
+    emailCadastro.classList.add("sucesso");
+  }
+
+  // Verifica se o campo de senha está vazio e aplica as classes de validação
+  if (senha === "") {
+    labelSenhaCadastr.classList.add("invalidoLabel");
+    senhaCadastr.classList.add("invalido");
+    emailValido = false;
+  } else {
+    labelSenhaCadastr.classList.remove("invalidoLabel");
+    senhaCadastr.classList.remove("invalido");
+    labelSenhaCadastr.classList.add("sucessoLabel");
+    senhaCadastr.classList.add("sucesso");
+  }
+
+  // Verifica se todos os campos estão preenchidos
+  if (nome !== "" && email !== "" && senha !== "") {
+    const usuarios = {
+      nome,
+      senha,
+      email,
+    };
+    cadastrarUsuario(usuarios);
   }
 });
